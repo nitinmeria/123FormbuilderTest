821e0fc382edd482904031f4cfd5e8afe824c731
nitinmeria <meria.nitin@gmail.com>
2023-12-07 22:49:09 +0530

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,3 +1,8 @@
-(function () {
-  window.alert("Hello! I am an alert box!");
-})();
+    window._adftrack = Array.isArray(window._adftrack) ? window._adftrack : (window._adftrack ? [window._adftrack] : []);
+    window._adftrack.push({
+        HttpHost: 'a2.adform.net',
+        pm: 3096574,
+        divider: encodeURIComponent('|'),
+        pagename: encodeURIComponent('Great Jobs KC_Thank You Page_Fy23')
+    });
+    (function () { var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://s2.adform.net/banners/scripts/st/trackpoint-async.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); })();
