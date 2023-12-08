adc58bc4c6a3388b8c552eb552d3240ec70756dd
nitinmeria <meria.nitin@gmail.com>
2023-12-08 22:55:05 +0530

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,8 +1,10 @@
-    window._adftrack = Array.isArray(window._adftrack) ? window._adftrack : (window._adftrack ? [window._adftrack] : []);
-    window._adftrack.push({
-        HttpHost: 'a2.adform.net',
-        pm: 3096574,
-        divider: encodeURIComponent('|'),
-        pagename: encodeURIComponent('Great Jobs KC_Thank You Page_Fy23')
-    });
-    (function () { var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://s2.adform.net/banners/scripts/st/trackpoint-async.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); })();
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1537674833419841');
+fbq('track', 'PageView');
